2bdab219a94963ec3b3cf4568cae93cba6b1882a
Juliet <113483666+juliet-jskim@users.noreply.github.com>
2023-01-29 15:18:57 -0800

    initial commit

---
+++ .history/pages/index_20230129135919.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '@/styles/Home.module.css'
+import Link from 'next/link'
+import { Main } from 'next/document'
+
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <main className="styles.container">
+      <nav>
+      <Link href="/about">About</Link>
+    </nav>
+    </main>
+    
+  )
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+## Description 
+Dynamic design week 5 playwright testing on different devices examples.
+
+## Languages
+Next.js 13
+CSS
+JSON
+
+## Dependencies
+playwright
